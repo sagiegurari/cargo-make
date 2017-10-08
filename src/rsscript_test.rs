867bdc416f0cd7457efe2e9227e5593864d93529
sagie gur ari <sagiegurari@gmail.com>
2017-10-08 13:56:52 +0000

    windows build

--- src/rsscript_test.rs
+++ src/rsscript_test.rs
@@ -1,39 +1,40 @@
 use super::*;
+use environment::is_ci;
 use environment::rustinfo;
 use types::RustChannel;
 
-#[test]
-#[cfg(target_os = "linux")]
-fn execute_valid() {
+fn should_test(panic_if_false: bool) -> bool {
     let rust_info = rustinfo::load();
+    let rust_channel = rust_info.channel.unwrap();
 
-    if rust_info.channel.unwrap() == RustChannel::Nightly {
+    if (cfg!(target_os = "linux") && rust_channel == RustChannel::Nightly) || !is_ci() {
+        true
+    } else if panic_if_false {
+        panic!("Skippied");
+    } else {
+        false
+    }
+}
+
+#[test]
+fn execute_valid() {
+    if should_test(false) {
         execute(&vec!["fn main() {println!(\"test\");}".to_string()]);
     }
 }
 
 #[test]
-#[cfg(target_os = "linux")]
 #[should_panic]
 fn execute_not_compile() {
-    let rust_info = rustinfo::load();
-
-    if rust_info.channel.unwrap() == RustChannel::Nightly {
+    if should_test(true) {
         execute(&vec!["fn main() {donotcompile();}".to_string()]);
-    } else {
-        panic!("skipped");
     }
 }
 
 #[test]
-#[cfg(target_os = "linux")]
 #[should_panic]
 fn execute_runtime_panic() {
-    let rust_info = rustinfo::load();
-
-    if rust_info.channel.unwrap() == RustChannel::Nightly {
+    if should_test(true) {
         execute(&vec!["fn main() {panic!(\"error\");}".to_string()]);
-    } else {
-        panic!("skipped");
     }
 }
