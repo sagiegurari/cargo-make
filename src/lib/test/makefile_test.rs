b4f7cedd817d309a8812fdb4551113bd1e549bec
sagie gur ari <sagiegurari@gmail.com>
2019-01-23 21:33:20 +0200

    Test internal makefile coverage tasks #183

--- src/lib/test/makefile_test.rs
+++ src/lib/test/makefile_test.rs
@@ -1,3 +1,4 @@
+use super::*;
 use crate::condition;
 use crate::descriptor;
 use crate::runner;
@@ -35,36 +36,39 @@ fn create_flow_info(config: &Config) -> FlowInfo {
 }
 
 #[test]
-#[cfg(target_os = "linux")]
 fn makefile_coverage_test() {
-    let config = load_descriptor();
-    let task = get_task("coverage", &config);
-    let run_task_info = task.run_task.unwrap();
+    if is_linux() {
+        let config = load_descriptor();
+        let task = get_task("coverage", &config);
+        let run_task_info = task.run_task.unwrap();
 
-    match run_task_info {
-        RunTaskInfo::Routing(ref routing_info) => {
-            let flow_info = create_flow_info(&config);
-            let task_name = runner::get_sub_task_name_for_routing_info(&flow_info, routing_info);
-            assert_eq!(task_name.unwrap(), "coverage-kcov");
-        }
-        _ => panic!("makefile error"),
-    };
+        match run_task_info {
+            RunTaskInfo::Routing(ref routing_info) => {
+                let flow_info = create_flow_info(&config);
+                let task_name =
+                    runner::get_sub_task_name_for_routing_info(&flow_info, routing_info);
+                assert_eq!(task_name.unwrap(), "coverage-kcov");
+            }
+            _ => panic!("makefile error"),
+        };
+    }
 }
 
 #[test]
-#[cfg(target_os = "linux")]
 fn makefile_ci_coverage_flow_test() {
-    let config = load_descriptor();
-    let task = get_task("ci-coverage-flow", &config);
-    let flow_info = create_flow_info(&config);
-    let step = Step {
-        name: "ci-coverage-flow".to_string(),
-        config: task,
-    };
+    if is_linux() {
+        let config = load_descriptor();
+        let task = get_task("ci-coverage-flow", &config);
+        let flow_info = create_flow_info(&config);
+        let step = Step {
+            name: "ci-coverage-flow".to_string(),
+            config: task,
+        };
 
-    let enabled = condition::validate_condition_for_step(&flow_info, &step);
+        let enabled = condition::validate_condition_for_step(&flow_info, &step);
 
-    assert_eq!(flow_info.env_info.ci_info.ci, enabled);
+        assert_eq!(flow_info.env_info.ci_info.ci, enabled);
+    }
 }
 
 #[test]
--- src/lib/test/mod.rs
+++ src/lib/test/mod.rs
@@ -8,6 +8,14 @@ use std::env;
 use std::fs::{create_dir_all, remove_dir_all};
 use std::path::PathBuf;
 
+pub(crate) fn is_linux() -> bool {
+    if cfg!(target_os = "linux") {
+        true
+    } else {
+        false
+    }
+}
+
 fn is_travis_ci() -> bool {
     match env::var("TRAVIS") {
         Ok(value) => value == "true",
@@ -27,7 +35,7 @@ pub(crate) fn should_test(panic_if_false: bool) -> bool {
     let rustinfo = rust_info::get();
     let rust_channel = rustinfo.channel.unwrap();
 
-    if (cfg!(target_os = "linux") && rust_channel == RustChannel::Nightly) || !ci_info::is_ci() {
+    if (is_linux() && rust_channel == RustChannel::Nightly) || !ci_info::is_ci() {
         true
     } else if panic_if_false {
         panic!("Skippied");
