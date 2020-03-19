f921772866af58f34a9d45aea65bccea492f35af
Maximilian Bosch <maximilian@mbosch.me>
2020-03-19 21:38:41 +0100

    Ensure that the specified rustup toolchain exists before using it
    Given a config like this
    
        [tasks.test]
        command = "cargo"
        args = [ "build" ]
        toolchain = "alarm"
    
    you get a fairly unintuitive error output like this:
    
    ```
    [cargo-make] INFO - Running Task: test
    [cargo-make] ERROR - Error while executing command, exit code: 1
    [cargo-make] WARN - Build Failed.
    ```
    
    This can be quite confusing especially for people who are new to a
    project and might've missed installing the specified rustup toolchain
    before building the project with `cargo-make`.
    
    This patch ensures in the `toolchain`-subcrate that the toolchain
    actually exists (by checking the exit-code of `rustup run <toolchain>
    true`) before running the actual task. If the toolchain doesn't exist,
    an error like this will be thrown:
    
    ```
    [cargo-make] INFO - Running Task: test
    [cargo-make] ERROR - Missing toolchain alarm! Please install it using rustup.
    [cargo-make] WARN - Build Failed.
    ```

--- src/lib/toolchain_test.rs
+++ src/lib/toolchain_test.rs
@@ -1,36 +1,46 @@
 use super::*;
+use envmnt;
+
+#[test]
+#[should_panic]
+fn wrap_command_invalid_toolchain() {
+    wrap_command("invalid-chain", "true", &None);
+}
 
 #[test]
 fn wrap_command_none_args() {
-    let output = wrap_command("mychain", "testcommand", &None);
+    let channel = envmnt::get_or_panic("CARGO_MAKE_RUST_CHANNEL");
+    let output = wrap_command(&channel, "true", &None);
 
     assert_eq!(output.command, "rustup".to_string());
 
     let args = output.args.unwrap();
     assert_eq!(args.len(), 3);
     assert_eq!(args[0], "run".to_string());
-    assert_eq!(args[1], "mychain".to_string());
-    assert_eq!(args[2], "testcommand".to_string());
+    assert_eq!(args[1], channel);
+    assert_eq!(args[2], "true".to_string());
 }
 
 #[test]
 fn wrap_command_empty_args() {
-    let output = wrap_command("mychain", "testcommand", &Some(vec![]));
+    let channel = envmnt::get_or_panic("CARGO_MAKE_RUST_CHANNEL");
+    let output = wrap_command(&channel, "true", &Some(vec![]));
 
     assert_eq!(output.command, "rustup".to_string());
 
     let args = output.args.unwrap();
     assert_eq!(args.len(), 3);
     assert_eq!(args[0], "run".to_string());
-    assert_eq!(args[1], "mychain".to_string());
-    assert_eq!(args[2], "testcommand".to_string());
+    assert_eq!(args[1], channel);
+    assert_eq!(args[2], "true".to_string());
 }
 
 #[test]
 fn wrap_command_with_args() {
+    let channel = envmnt::get_or_panic("CARGO_MAKE_RUST_CHANNEL");
     let output = wrap_command(
-        "mychain",
-        "testcommand",
+        &channel,
+        "true",
         &Some(vec!["echo".to_string(), "test".to_string()]),
     );
 
@@ -39,8 +49,8 @@ fn wrap_command_with_args() {
     let args = output.args.unwrap();
     assert_eq!(args.len(), 5);
     assert_eq!(args[0], "run".to_string());
-    assert_eq!(args[1], "mychain".to_string());
-    assert_eq!(args[2], "testcommand".to_string());
+    assert_eq!(args[1], channel);
+    assert_eq!(args[2], "true".to_string());
     assert_eq!(args[3], "echo".to_string());
     assert_eq!(args[4], "test".to_string());
 }
