sagiegurari/cargo-make

commit fa929b116f78c1b05da5c8f7684352566035719d

Fix workspace detection for sub flows

src/lib/execution_plan.rs
@@ -288,9 +288,11 @@ fn is_workspace_flow(
     task: &str,
     disable_workspace: bool,
     crate_info: &CrateInfo,
+    sub_flow: bool,
 ) -> bool {
     // if project is not a workspace or if workspace is disabled via cli, return no workspace flow
     if disable_workspace
+        || sub_flow
         || (crate_info.workspace.is_none() && !envmnt::is("CARGO_MAKE_WORKSPACE_EMULATION"))
         || envmnt::exists("CARGO_MAKE_CRATE_CURRENT_WORKSPACE_MEMBER")
     {
@@ -385,7 +387,8 @@ pub(crate) fn create(
     // load crate info and look for workspace info
     let crate_info = environment::crateinfo::load();
 
-    let workspace_flow = is_workspace_flow(&config, &task, disable_workspace, &crate_info);
+    let workspace_flow =
+        is_workspace_flow(&config, &task, disable_workspace, &crate_info, sub_flow);
 
     if workspace_flow {
         let workspace_task = create_workspace_task(crate_info, task);

examples/workspace2/Makefile.toml
@@ -5,6 +5,9 @@ CARGO_MAKE_EXTEND_WORKSPACE_MAKEFILE = "true"
 [tasks.echo]
 script = ["echo hello from workspace"]
 
+[tasks.echo-member]
+script = ["echo hello from member"]
+
 [tasks.echo_top_and_members]
 workspace = false
 dependencies = [ "echo" ]
@@ -29,4 +32,12 @@ script = ["echo root at: CARGO_MAKE_WORKSPACE_WORKING_DIRECTORY=${CARGO_MAKE_WOR
 workspace = false
 dependencies = ["workspace_root"]
 run_task = { name = "workspace_root", fork = true }
+
+[tasks.echo_run_task]
+run_task = "echo"
+
+[tasks.composite_with_run_task]
+workspace = false
+dependencies = ["echo_run_task"]
+run_task = { name = "echo-member", fork = true }
 

commit 1176842a1266fa8ea342dfc8bcf25e3fd9c1d4f4

Allow for explicit workspace flow in sub flows
Commit fa929b116f78 ("Fix workspace detection for sub flows") caused a
regression when having a root workspace with tasks that relies on each
member to define CARGO_MAKE_CARGO_BUILD_TEST_FLAGS with unique set of
features.

Determine if workspace flow is explicitly set in the task when planning
the execution and take this setting into account when running in a sub
flow.

src/lib/execution_plan.rs
@@ -290,21 +290,32 @@ fn is_workspace_flow(
     crate_info: &CrateInfo,
     sub_flow: bool,
 ) -> bool {
+    // determine if workspace flow is explicitly set and enabled in the requested task
+    let (task_set_workspace, task_enable_workspace) =
+        match get_optional_normalized_task(config, task, true) {
+            Some(normalized_task) => match normalized_task.workspace {
+                Some(enable_workspace) => (true, enable_workspace),
+                None => (false, false),
+            },
+            None => (false, false),
+        };
+
     // if project is not a workspace or if workspace is disabled via cli, return no workspace flow
     if disable_workspace
-        || sub_flow
+        || (sub_flow && !task_enable_workspace)
         || (crate_info.workspace.is_none() && !envmnt::is("CARGO_MAKE_WORKSPACE_EMULATION"))
         || envmnt::exists("CARGO_MAKE_CRATE_CURRENT_WORKSPACE_MEMBER")
     {
         false
     } else {
-        // check for configured default workspace flag
-        let default_to_workspace = config.config.default_to_workspace.unwrap_or(true);
-
         // project is a workspace and wasn't disabled via cli, need to check requested task
-        match get_optional_normalized_task(config, task, true) {
-            Some(cli_task) => cli_task.workspace.unwrap_or(default_to_workspace),
-            None => default_to_workspace,
+
+        // use requested task's workspace flag if set
+        if task_set_workspace {
+            task_enable_workspace
+        } else {
+            // use configured default workspace flag if set
+            config.config.default_to_workspace.unwrap_or(true)
         }
     }
 }

src/lib/execution_plan_test.rs
@@ -583,6 +583,31 @@ fn is_workspace_flow_true_in_task() {
 }
 
 #[test]
+fn is_workspace_flow_default_false_in_task_and_sub_flow() {
+    let mut crate_info = CrateInfo::new();
+    let members = vec![];
+    crate_info.workspace = Some(Workspace {
+        members: Some(members),
+        exclude: None,
+    });
+
+    let task = Task::new();
+
+    let mut config = Config {
+        config: ConfigSection::new(),
+        env_files: vec![],
+        env: IndexMap::new(),
+        env_scripts: vec![],
+        tasks: IndexMap::new(),
+    };
+    config.tasks.insert("test".to_string(), task);
+
+    let workspace_flow = is_workspace_flow(&config, "test", false, &crate_info, true);
+
+    assert!(!workspace_flow);
+}
+
+#[test]
 fn is_workspace_flow_true_in_task_and_sub_flow() {
     let mut crate_info = CrateInfo::new();
     let members = vec![];
@@ -593,6 +618,32 @@ fn is_workspace_flow_true_in_task_and_sub_flow() {
 
     let mut task = Task::new();
     task.workspace = Some(true);
+
+    let mut config = Config {
+        config: ConfigSection::new(),
+        env_files: vec![],
+        env: IndexMap::new(),
+        env_scripts: vec![],
+        tasks: IndexMap::new(),
+    };
+    config.tasks.insert("test".to_string(), task);
+
+    let workspace_flow = is_workspace_flow(&config, "test", false, &crate_info, true);
+
+    assert!(workspace_flow);
+}
+
+#[test]
+fn is_workspace_flow_false_in_task_and_sub_flow() {
+    let mut crate_info = CrateInfo::new();
+    let members = vec![];
+    crate_info.workspace = Some(Workspace {
+        members: Some(members),
+        exclude: None,
+    });
+
+    let mut task = Task::new();
+    task.workspace = Some(false);
 
     let mut config = Config {
         config: ConfigSection::new(),