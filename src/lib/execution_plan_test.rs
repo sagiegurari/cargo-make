5d5bb3cb33887b6b3d2f0d2a055248a6352a53a6
Alexander Lyon <arlyon@me.com>
2020-12-02 17:08:56 +0100

    Add test

--- src/lib/execution_plan_test.rs
+++ src/lib/execution_plan_test.rs
@@ -1,6 +1,7 @@
 use super::*;
 use crate::types::{
-    ConfigSection, CrateInfo, PlatformOverrideTask, Task, TaskWatchOptions, Workspace,
+    ConfigSection, CrateInfo, DependencyIdentifier, PlatformOverrideTask, Task, TaskWatchOptions,
+    Workspace,
 };
 use indexmap::IndexMap;
 use std::env;
@@ -891,6 +892,50 @@ fn create_with_dependencies() {
     assert_eq!(execution_plan.steps.len(), 4);
     assert_eq!(execution_plan.steps[0].name, "init");
     assert_eq!(execution_plan.steps[1].name, "task_dependency");
+    assert_eq!(execution_plan.steps[2].name, "test");
+    assert_eq!(execution_plan.steps[3].name, "end");
+}
+
+#[test]
+fn create_with_foreign_dependencies() {
+    let mut config_section = ConfigSection::new();
+    config_section.init_task = Some("init".to_string());
+    config_section.end_task = Some("end".to_string());
+    let mut config = Config {
+        config: config_section,
+        env_files: vec![],
+        env: IndexMap::new(),
+        env_scripts: vec![],
+        tasks: IndexMap::new(),
+    };
+
+    config.tasks.insert("init".to_string(), Task::new());
+    config.tasks.insert("end".to_string(), Task::new());
+
+    let mut task = Task::new();
+    task.dependencies = Some(vec![DependencyIdentifier::Definition(TaskIdentifier {
+        name: "task_dependency".to_string(),
+        path: Some("other_file.toml".to_string()),
+    })]);
+
+    let task_dependency = Task::new();
+
+    config.tasks.insert("test".to_string(), task);
+    config
+        .tasks
+        .insert("task_dependency".to_string(), task_dependency);
+
+    let execution_plan = create(&config, "test", false, true, false);
+    assert_eq!(execution_plan.steps.len(), 4);
+    assert_eq!(execution_plan.steps[0].name, "init");
+    assert_eq!(
+        execution_plan.steps[1].name,
+        "other_file.toml:task_dependency"
+    );
+    assert_eq!(
+        execution_plan.steps[1].config.cwd,
+        Some("other_file.toml".to_string())
+    );
     assert_eq!(execution_plan.steps[2].name, "test");
     assert_eq!(execution_plan.steps[3].name, "end");
 }
