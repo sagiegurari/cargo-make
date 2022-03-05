c79240af24c3bfc96ab1312bb9f5aae6c97657a9
sagie gur ari <sagiegurari@gmail.com>
2022-03-05 19:24:19 +0000

    Fix: detect workspace member for non virtual workspaces #636

--- examples/workspace_crate/target/debug/deps/example-004bfe9e0e809f5f.d
+++
@@ -1,7 +0,0 @@
-/home/pi/workspace/projects/rust/cargo-make/examples/workspace_crate/target/debug/deps/example-004bfe9e0e809f5f.rmeta: src/lib.rs
-
-/home/pi/workspace/projects/rust/cargo-make/examples/workspace_crate/target/debug/deps/libexample-004bfe9e0e809f5f.rlib: src/lib.rs
-
-/home/pi/workspace/projects/rust/cargo-make/examples/workspace_crate/target/debug/deps/example-004bfe9e0e809f5f.d: src/lib.rs
-
-src/lib.rs:
