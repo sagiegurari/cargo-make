2164f1e6d9cda851fa111a485f68a4c2a7b09ded
John Vandenberg <jayvdb@gmail.com>
2023-03-03 19:12:16 +0800

    Remove target/ build artifacts

--- examples/workspace/target/debug/deps/member1-98b0b3daf0f68025.d
+++
@@ -1,7 +0,0 @@
-/home/pi/workspace/projects/rust/cargo-make/examples/workspace/target/debug/deps/member1-98b0b3daf0f68025.rmeta: member1/src/lib.rs
-
-/home/pi/workspace/projects/rust/cargo-make/examples/workspace/target/debug/deps/libmember1-98b0b3daf0f68025.rlib: member1/src/lib.rs
-
-/home/pi/workspace/projects/rust/cargo-make/examples/workspace/target/debug/deps/member1-98b0b3daf0f68025.d: member1/src/lib.rs
-
-member1/src/lib.rs:
--- examples/workspace/target/debug/deps/member1-f37fbcf3e793e286.d
+++
@@ -1,5 +0,0 @@
-/home/pi/workspace/projects/rust/cargo-make/examples/workspace/target/debug/deps/member1-f37fbcf3e793e286.rmeta: member1/src/lib.rs
-
-/home/pi/workspace/projects/rust/cargo-make/examples/workspace/target/debug/deps/member1-f37fbcf3e793e286.d: member1/src/lib.rs
-
-member1/src/lib.rs:
