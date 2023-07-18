sagiegurari/cargo-make

commit fe5ca3aba1050d0bce39663e0bd270d2a7d83049

Fix: relative path detection for workspace members defined in dependencies #773

src/lib/environment/crateinfo.rs
@@ -142,6 +142,8 @@ fn get_members_from_dependencies(crate_info: &CrateInfo) -> Vec<String> {
                                 let member_path =
                                     path.chars().skip(2).take(path.len() - 2).collect();
                                 members.push(member_path);
+                            } else if Path::new(path).is_relative() {
+                                members.push(path.to_string());
                             }
                         }
                         None => (),
@@ -173,6 +175,15 @@ fn add_members(crate_info: &mut CrateInfo, new_members: Vec<String>) {
                 None => workspace.members = Some(new_members),
             },
             None => (), //not a workspace
+        }
+    }
+}
+
+fn dedup_members(crate_info: &mut CrateInfo) {
+    if let Some(ref mut workspace) = crate_info.workspace {
+        if let Some(ref mut members) = workspace.members {
+            members.sort();
+            members.dedup();
         }
     }
 }
@@ -221,6 +232,8 @@ fn load_workspace_members(crate_info: &mut CrateInfo) {
             let members_vec = vec![".".to_string()];
             add_members(crate_info, members_vec);
         }
+
+        dedup_members(crate_info);
 
         remove_excludes(crate_info);
     }

src/lib/environment/crateinfo_test.rs
@@ -223,12 +223,19 @@ fn get_members_from_dependencies_workspace_paths() {
     crate_info.dependencies = Some(dependencies);
     let members = get_members_from_dependencies(&crate_info);
 
-    assert_eq!(members.len(), 2);
-    let value1 = &members[0];
-    assert!((value1 == "member1") || value1 == "member2");
-    let value2 = &members[1];
-    assert!((value2 == "member1") || value2 == "member2");
-    assert!(value1 != value2);
+    assert_eq!(members.len(), 3);
+    assert!(members
+        .iter()
+        .position(|member| member == "member1")
+        .is_some());
+    assert!(members
+        .iter()
+        .position(|member| member == "member2")
+        .is_some());
+    assert!(members
+        .iter()
+        .position(|member| member == "somepath")
+        .is_some());
 }
 
 #[test]
@@ -571,7 +578,41 @@ fn load_workspace_members_mixed() {
         .position(|member| member == "examples/workspace/member1")
         .is_some());
     assert!(members.iter().position(|member| member == ".").is_some());
-    assert_eq!(members.len(), 8);
+    assert_eq!(members.len(), 9);
+}
+
+#[test]
+fn load_workspace_members_mixed_members_and_paths() {
+    let mut crate_info = CrateInfo::new();
+
+    let mut dependencies = IndexMap::new();
+    dependencies.insert(
+        "my_package2".to_string(),
+        CrateDependency::Info(CrateDependencyInfo {
+            path: Some("path/to/my_package2".to_string()),
+        }),
+    );
+    crate_info.dependencies = Some(dependencies);
+
+    let mut workspace = Workspace::new();
+    workspace.members = Some(vec!["path/to/my_package1".to_string()]);
+
+    crate_info.workspace = Some(workspace);
+    load_workspace_members(&mut crate_info);
+
+    assert!(crate_info.workspace.is_some());
+    workspace = crate_info.workspace.unwrap();
+    assert!(workspace.members.is_some());
+    let members = workspace.members.unwrap();
+    assert_eq!(members.len(), 2);
+    assert!(members
+        .iter()
+        .position(|member| member == "path/to/my_package1")
+        .is_some());
+    assert!(members
+        .iter()
+        .position(|member| member == "path/to/my_package2")
+        .is_some());
 }
 
 #[test]

commit 17489237c2099a03d2460c576bbabedd1ebe2967

crateinfo: do not sort workspace members
Official doc states following

> The order of the members is defined by the member attribute in the workspace Cargo.toml.

, but it was broken in commit fe5ca3aba1050d0bce39663e0bd270d2a7d83049.

Remove sorting and add new test-case to catch such problems in future

Signed-off-by: Pavel Skripkin <paskripkin@gmail.com>

src/lib/environment/crateinfo.rs
@@ -197,7 +197,6 @@ fn add_members(crate_info: &mut CrateInfo, new_members: Vec<String>) {
 fn dedup_members(crate_info: &mut CrateInfo) {
     if let Some(ref mut workspace) = crate_info.workspace {
         if let Some(ref mut members) = workspace.members {
-            members.sort();
             members.dedup();
         }
     }

src/lib/environment/crateinfo_test.rs
@@ -50,6 +50,24 @@ fn add_members_workspace_new_members_with_data() {
 }
 
 #[test]
+fn add_members_workspace_non_alpha_ordered() {
+    let mut crate_info = CrateInfo::new();
+
+    crate_info.workspace = Some(Workspace::new());
+    add_members(
+        &mut crate_info,
+        vec!["test2".to_string(), "test1".to_string()],
+    );
+
+    load_workspace_members(&mut crate_info);
+
+    let members = crate_info.workspace.unwrap().members.unwrap();
+    assert_eq!(members.len(), 2);
+    assert_eq!(members[0], "test2".to_owned());
+    assert_eq!(members[1], "test1".to_owned());
+}
+
+#[test]
 fn add_members_workspace_empty_members_with_data() {
     let mut crate_info = CrateInfo::new();
     let mut workspace = Workspace::new();