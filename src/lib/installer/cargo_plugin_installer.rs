f5cd42ac3178111518edf3750c42dec44cbf443b
sagie gur ari <sagiegurari@gmail.com>
2024-01-13 17:57:14 +0000

    debugging

--- src/lib/installer/cargo_plugin_installer.rs
+++ src/lib/installer/cargo_plugin_installer.rs
@@ -38,7 +38,7 @@ fn is_crate_in_list_output(crate_name: &str, output: &str) -> bool {
 
         let words: Vec<&str> = line.split(' ').collect();
         let plugin_name = words[0].trim();
-        bool found = plugin_name == crate_name
+        let found = plugin_name == crate_name
         debug!("Checking Line: {}\nPlugin: <{}> Expected: <{}> Found: {}", &line, &plugin_name, crate_name, found);
 
         if found {
